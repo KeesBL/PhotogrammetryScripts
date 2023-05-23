0aae51a348963c75bfff721ff6717472328e4998
Kees <kees.bl@gmail.com>
2023-05-23 14:54:27 -0700

    Modified to installed version

--- AddPhotosFromFolderRecursive.py
+++ AddPhotosFromFolderRecursive.py
@@ -5,25 +5,29 @@ from pathlib import Path
 # This script will add all JPG images in a folder
 # including all subfolders of that folder. 
 #
-# (c) Kees Beemster Leverenz 2022
+# Install by copying this script to %UserProfile%\AppData\Local\Agisoft\Metashape Pro\scripts
+#
+# Kees Beemster Leverenz 2022
 
-# Get folder path
-app = Metashape.app
-message = "Select folder that contains your photos. Photos must be in .jpg format. "
-message += "Photos in all subfolders will also be added to the current chunk."
-path = Metashape.app.getExistingDirectory(message)
+def AddPhotosFromFolderRecursive():
 
-# Get photos (rglob is recursive)
-caseInsensitiveJpgPattern = "*.[jJ][pP][gG]"
-photos = list(Path(path).rglob(caseInsensitiveJpgPattern))
+    # Get folder path
+    app = Metashape.app
+    message = "Select folder that contains your photos. Photos must be in .jpg format. "
+    message += "Photos in all subfolders will also be added to the current chunk."
+    path = Metashape.app.getExistingDirectory(message)
 
-# Generate list of full paths
-photoPaths = list()
-print("Loading photos...")
-for photo in photos:
-	photoPath = os.path.join(path,photo)
-	photoPaths.append(photoPath)
-	Metashape.app.update()
+    # Get photos (rglob is recursive)
+    caseInsensitiveJpgPattern = "*.[jJ][pP][gG]"
+    photos = list(Path(path).rglob(caseInsensitiveJpgPattern))
+
+    # Generate list of full paths
+    photoPaths = list()
+    print("Loading photos...")
+    for photo in photos:
+        photoPath = os.path.join(path,photo)
+        photoPaths.append(photoPath)
+        Metashape.app.update()
     
-# Add photos
-Metashape.app.document.chunk.addPhotos(photoPaths)
+    # Add photos
+    Metashape.app.document.chunk.addPhotos(photoPaths)
