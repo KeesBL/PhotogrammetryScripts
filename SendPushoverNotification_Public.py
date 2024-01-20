b3927408dee855a9c7e9b4f288e1b79f620b6f4b
Kees <kees.bl@gmail.com>
2024-01-20 13:10:42 -0800

    Update SendPushoverNotification_Public.py

--- SendPushoverNotification_Public.py
+++ SendPushoverNotification_Public.py
@@ -1,12 +1,12 @@
 import http.client, urllib, sys
 
-# You will need to modify this section. Obtain a user key by ...
+# You will need to modify this section. Obtain an app key by signing up for Pushover, and locating the User Key in the "Your User Key" section of https://pushover.net/#apps. It is a string of 30 random letters and numbers. 
 userKey = "YourUserKeyHere"
 
-# You will need to modify this section. Obtain an app key by ...
+# You will need to modify this section. Obtain a user key by registering an application under "Your Applications" section of https://pushover.net/#apps. Once complete, you can click the name of your application on https://pushover.net/#apps, and your API token will be displayed. It is a string of 30 random letters and numbers. 
 appToken = "YourAppKeyHere"
 
-# Message -- for this script, the first argument specified
+# Message -- for this script, the first argument specified in the prompt or in the batch script. 
 message = sys.argv[1]
 
 # Send push notification
